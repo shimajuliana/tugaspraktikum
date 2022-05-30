f0416e5c058acda32b17e01823631d736c9ab005
HarisMmaulana <93259641+shimajuliana@users.noreply.github.com>
2022-05-30 19:19:22 +0800

    update data

--- pages/routes.php
+++ pages/routes.php
@@ -1,13 +1,19 @@
-<?php 
-if(isset($_GET['page'])){
+<?php
+if (isset($_GET['page'])) {
     $page = $_GET['page'];
-    switch ($page) {
+    switch($page){
         case '':
         case 'dashboard':
             file_exists("dashboard.php") ? include "dashboard.php" : include "404.php";
             break;
         case 'dosen':
-            file_exists("dosen.php") ? include "dosen.php" : include "404.php";
+            file_exists("dosen/dosen.php") ? include "dosen/dosen.php" : include "404.php";
+            break;
+        case 'dosencreate':
+            file_exists("dosen/dosencreate.php") ? include "dosen/dosencreate.php" : include "404.php";
+            break;
+        case 'dosenupdate':
+            file_exists("dosen/dosenupdate.php") ? include "dosen/dosenupdate.php" : include "404.php";
             break;
         case 'matakuliah':
             file_exists("matakuliah.php") ? include "matakuliah.php" : include "404.php";
@@ -18,7 +24,7 @@ if(isset($_GET['page'])){
         default:
             include "404.php";
             break;
-    } 
+    }
 } else {
     include "dashboard.php";
 }

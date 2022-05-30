a616b6e167a2f9c9bf8802a16b62810addbd0721
HarisMmaulana <93259641+shimajuliana@users.noreply.github.com>
2022-05-30 19:11:36 +0800

    menampilkan dosen

--- database/new_dosen.sql
+++ database/new_dosen.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `praktikum_daftartugas6m`.`dosen` ( `id` INT NOT NULL AUTO_INCREMENT , `nama_dosen` INT(255) NOT NULL , `handphone` INT(255) NULL , `email` INT(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `praktikum_daftartugas6m`.`dosen` ( `id` INT NOT NULL AUTO_INCREMENT , `nama_dosen` VARCHAR(255) NOT NULL , `handphone` VARCHAR(255) NULL , `email` VARCHAR(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
